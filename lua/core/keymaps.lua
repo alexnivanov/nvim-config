7ab3a0a21633e0426cea416f675879a8911661af
Alexander Ivanov <alexander.n.ivanov@yandex.ru>
2024-04-19 15:58:42 +0300

    New key bindings

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,11 +1,11 @@
--- Navigate vim panes better
+-- Navigate vim panes
 vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
 vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
 vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+vim.keymap.set('n', '<c-w>', ':q<CR>')
 
--- Clear search highlighting
-vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>', { desc = 'Clear search highlighting' })
 
--- Quit all without saving
-vim.keymap.set('n', '<leader>q', ':qa<CR>')
+vim.keymap.set('n', '<leader>q', ':qa<CR>', { desc = 'Quit all' })
+vim.keymap.set('n', '<leader>w', ':q<CR>', { desc = 'Close' })
