07399abdd4fb2ab2a1c00dbc3a49b7b4a97b5361
Alexander Ivanov <alexander.n.ivanov@yandex.ru>
2024-04-22 11:38:20 +0300

    Add git-blame toggle hotkey

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -5,6 +5,8 @@ vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 vim.keymap.set('n', '<c-w>', ':q<CR>')
 
+vim.keymap.set('n', '<c-b>', ':GitBlameToggle<CR>', { desc = 'Toggle git blame' })
+
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>', { desc = 'Clear search highlighting' })
 
 vim.keymap.set('n', '<leader>q', ':qa<CR>', { desc = 'Quit all' })
