10e3d2b7a76b96e2e0387631bee8d39df61f10fe
Alexander Ivanov <alexander.n.ivanov@yandex.ru>
2024-05-15 11:50:59 +0300

    Adjust open diagnostic command

--- lua/core/plugin_config/lsp.lua
+++ lua/core/plugin_config/lsp.lua
@@ -2,7 +2,7 @@
 
 -- note: diagnostics are not exclusive to lsp servers
 -- so these can be global keybindings
-vim.keymap.set('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>')
+vim.keymap.set('n', '<leader>d', '<cmd>lua vim.diagnostic.open_float()<cr>', { desc = 'Open diagnostic float' })
 vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>')
 vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>')
 
