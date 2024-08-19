75e17ebfbc6e26be3db17ad1341f43a302a713c8
Alexander Ivanov <alexander.n.ivanov@yandex.ru>
2024-08-19 12:08:33 +0300

    Revive barbar

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -6,6 +6,12 @@ vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 
 vim.keymap.set('n', '<c-q>', ':q<CR>')
 
+-- Barbar
+vim.keymap.set('n', '<c-w>', ':BufferClose<CR>')
+vim.keymap.set('n', '[t', ':BufferPrevious<cr>')
+vim.keymap.set('n', ']t', ':BufferNext<cr>')
+
+
 vim.keymap.set('n', '<c-b>', ':GitBlameToggle<CR>', { desc = 'Toggle git blame' })
 
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>', { desc = 'Clear search highlighting' })
